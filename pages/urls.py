4662dee471de393913b3fafd45e1a3a898aa8ade
SYST202u12 <SYST202u12@ad.pcc.edu>
2020-08-26 10:24:11 -0700

    add ee, cive pages, bootswatch materia theme and some jumbotron css for the home page

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 # pages/urls.py
 
 from django.urls import path
-from .views import HomePageView, MePageView
+from .views import HomePageView, MePageView, CivePageView, EePageView
 
 urlpatterns = [
     path("me/", MePageView.as_view(), name="me"),
+    path("cive/", CivePageView.as_view(), name="cive"),
+    path("ee/", EePageView.as_view(), name="ee"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,11 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-
 class MePageView(TemplateView):
     template_name = "me.html"
+
+class CivePageView(TemplateView):
+    template_name = "cive.html"
+
+class EePageView(TemplateView):
+    template_name = "ee.html"
