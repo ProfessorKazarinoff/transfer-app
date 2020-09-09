6f1ea9cc16374bd9d7f703cc1d7a783f5fcad72e
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2020-09-08 21:27:05 -0700

    changed STATIC_ROOT

--- transfer_project/settings.py
+++ transfer_project/settings.py
@@ -124,7 +124,7 @@ AUTH_USER_MODEL = "users.CustomUser"  # new
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [Path(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_COOKIE_SECURE = True
